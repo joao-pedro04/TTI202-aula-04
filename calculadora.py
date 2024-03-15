d32a639176bc8136e8d4fca687471612a35e4951
joao-pedro04 <107186628+joao-pedro04@users.noreply.github.com>
2024-03-14 21:56:31 -0300

    Update calculadora.py
    Criação das seleções do usuário e finalização da calculadora

--- calculadora.py
+++ calculadora.py
@@ -12,3 +12,25 @@ def divisao(x,y):
         print("Erro! Não é possível dividir por zero.")
     else:
         return x / y
+    
+print("Selecione uma opcao:")
+print("1. Adição")
+print("2. Subtração")
+print("3. Multiplicacao")
+print("4. Divisao")
+
+escolha = input("Digite a sua escolha (1/2/3/4): ")
+
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+
+if escolha == '1':
+    print(num1, "+", num2, "=", adicao(num1, num2))
+elif escolha == '2':
+    print(num1, "-", num2, "=", subtracao(num1, num2))
+elif escolha == '3':
+    print(num1, "*", num2, "=", multiplicacao(num1, num2))
+elif escolha == '4':
+    print(num1, "/", num2, "=", divisao(num1, num2))
+else:
+    print("Opcao invalida! Tente novamente.")
