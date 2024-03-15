371172ade6ebcf0204000318c6998ba6f791173b
joao-pedro04 <107186628+joao-pedro04@users.noreply.github.com>
2024-03-14 21:49:20 -0300

    Criação das funções da calculadora
    Funções da calculadora

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+def adicao(x,y):
+    return x + y
+
+def subtracao(x,y):
+    return x - y
+
+def multiplicacao(x,y):
+    return x * y
+
+def divisao(x,y):
+    if  y == 0:
+        print("Erro! Não é possível dividir por zero.")
+    else:
+        return x / y
